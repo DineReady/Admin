fa1793b1410d51869af06dbdbe324062be11d360
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-03 12:08:43 +0100

    Refactor mock data in App.tsx and index.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "./index.css";
 import { CreateOrder, Header, Main } from "./components";
-import { IOrder, OrderStatus } from "./types";
+import { mock } from "./lib/mocks";
 
 export default function App(): JSX.Element {
-    const mock: IOrder[] = [
-        { status: OrderStatus.Active, id: "1" },
-        { status: OrderStatus.Pending, id: "2" },
-        { status: OrderStatus.Complete, id: "3" },
-    ];
-
     return (
         <>
             <Header orders={mock.length} />
