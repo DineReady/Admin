04ef10614fed69a7619ce382bcc3e535f1132b67
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-03 20:37:03 +0100

    Add body-parser and cors dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port: number = 8080;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/api", (req: Request, res: Response) => {
     res.send("Hello World!");
