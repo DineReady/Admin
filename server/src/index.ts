6356379128520dc20852d0b72e1b57125569271c
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-03 20:53:45 +0100

    Remove unused endpoint and add TODO comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,10 +17,6 @@ app.get("/api", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
-app.get("/huj", (req: Request, res: Response) => {
-    res.send("Huj!");
-});
-
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
