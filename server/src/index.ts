0137df1c7b514cf0a362c090e61aff36651ed69f
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-03 21:36:11 +0100

    Update eslint configuration and add SQLite database connection and table creation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,34 @@
+/* eslint-disable quotes */
 import express, { Express, Request, Response } from "express";
+import sqlite, { Database } from "sqlite3";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
+sqlite.verbose();
 
 const app: Express = express();
 const port: number = 8080;
+const dbPath: string = path.resolve(__dirname, "db", "dine_ready.db");
+
+// CONNECT TO DATABASE
+const db: Database = new sqlite.Database(
+    dbPath,
+    sqlite.OPEN_READWRITE,
+    (err: Error | null) => {
+        if (err) console.error(err.message);
+        else console.log("[server] Connected to the dine_ready database");
+    },
+);
+
+// CREATE TABLES
+const sql = `CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, status TEXT)`;
+db.run(sql, (err: Error | null) => {
+    if (err) console.error(err.message);
+    else console.log("[server] Table created");
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +40,7 @@ app.get("/api", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server] Server is running at http://localhost:${port}`);
 });
 
 //TODO: proxy in FE doesnt work
