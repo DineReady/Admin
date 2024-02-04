65b3af17d04e944250c7209e272c46fb1f5111ca
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-04 12:16:47 +0100

    Refactor server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,25 +9,24 @@ import path from "path";
 dotenv.config();
 sqlite.verbose();
 
+const db_file_name: string = "dine_ready.db";
+const db_dir_name: string = "db";
 const app: Express = express();
-const port: number = 8080;
-const dbPath: string = path.resolve(__dirname, "db", "dine_ready.db");
-
-// CONNECT TO DATABASE
+const PORT: number = 8080;
+const db_path: string = path.resolve(__dirname, db_dir_name, db_file_name);
 const db: Database = new sqlite.Database(
-    dbPath,
+    db_path,
     sqlite.OPEN_READWRITE,
-    (err: Error | null) => {
+    (err: Error | null): void => {
         if (err) console.error(err.message);
-        else console.log("[server] Connected to the dine_ready database");
+        console.log(`[server] Connected to the ${db_file_name}`);
     },
 );
+const sql: string = `CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, status TEXT)`;
 
-// CREATE TABLES
-const sql = `CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, status TEXT)`;
-db.run(sql, (err: Error | null) => {
+db.run(sql, (err: Error | null): void => {
     if (err) console.error(err.message);
-    else console.log("[server] Table created");
+    console.log("[server] Table created");
 });
 
 app.use(cors());
@@ -35,12 +34,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.get("/api", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response): void => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log(`[server] Server is running at http://localhost:${port}`);
+app.listen(PORT, (): void => {
+    console.log(`[server] Server is running at http://localhost:${PORT}`);
 });
 
 //TODO: proxy in FE doesnt work
