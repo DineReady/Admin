13fdb5f70a5e3bb612b44766d8c7a10ec1092a81
Gorbkos <gniewkoscielak@gmail.com>
2024-02-07 10:55:16 +0100

    button fix

--- client/src/components/orders/CreateOrder.tsx
+++ client/src/components/orders/CreateOrder.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CreateOrder = () => {
     return (
-        <button className="absolute h-20 w-20 rounded-full bg-yellow-200 flex items-center justify-center bottom-1 right-1">
+        <button className="fixed h-20 w-20 rounded-full bg-yellow-200 flex items-center justify-center bottom-3 right-3 transition-all ease-in-out hover:bg-yellow-300 duration-500">
             <Plus size={30} />
         </button>
     );
