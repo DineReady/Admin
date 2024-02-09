553e91538825a1e0986b90e711315dd43f70d822
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-09 14:39:26 +0100

    Refactor createOrder function and add order insertion

--- server/src/routes/create_order.ts
+++ server/src/routes/create_order.ts
@@ -1,45 +1,8 @@
-// import { Request, Response } from "express";
-// import { v4 as uuidv4 } from "uuid";
-// import { db } from "../db";
-
-// export default async function createOrder(req: Request, res: Response): Promise<void> {
-//     let uniqueId: string = uuidv4();
-
-//     try {
-//         db.all("SELECT id FROM orders", async (err: Error | null, rows: { id: string }[]) => {
-//             if (err) {
-//                 console.error(err.message);
-//                 return res.status(500).send(err);
-//             }
-
-//             const existingOrderIds: string[] = rows.map((row: { id: string }) => row.id);
-//             let isUnique = !existingOrderIds.includes(uniqueId);
-
-//             while (!isUnique) {
-//                 uniqueId = uuidv4();
-//                 isUnique = !existingOrderIds.includes(uniqueId);
-//             }
-
-//             // db.run("INSERT INTO orders (id, status) VALUES (?, ?)", [uniqueId, "pending"], (err: Error | null) => {
-//             //     if (err) {
-//             //         console.error(err.message);
-//             //         return res.status(500).send(err);
-//             //     }
-//             //     console.log(`[server] Order with ID ${uniqueId} added`);
-//             //     res.status(200).json({ uniqueId });
-//             // });
-//         });
-//     } catch (error: unknown) {
-//         console.error((error as Error).message);
-//         res.status(500).send(error);
-//     }
-// }
 import { Request, Response } from "express";
 import { db } from "../db";
 import { v4 as uuidv4 } from "uuid";
 
-export default async function 
-createOrder(req: Request, res: Response): Promise<void> {
+export default async function createOrder(req: Request, res: Response): Promise<void> {
     let uniqueId: string = uuidv4();
 
     try {
@@ -56,12 +19,11 @@ createOrder(req: Request, res: Response): Promise<void> {
                 isUnique = !existingOrderIds.includes(uniqueId);
             }
 
+            db.run("INSERT INTO orders (id, status) VALUES (?, ?)", [uniqueId, "pending"], (err: Error | null) => {
+                if (err) throw `Error inserting order: ${err.message}`;
+                console.log(`[server] Order with ID ${uniqueId} inserted`);
+            });
             res.status(200).json({ uniqueId });
-        });
-
-        db.run("INSERT INTO orders (id, status) VALUES (?, ?)", [uniqueId, "pending"], (err: Error | null) => {
-            if (err) throw err;
-            console.log(`[server] Order with ID ${uniqueId} added`);
         });
     } catch (error: unknown) {
         console.error((error as Error).message);
