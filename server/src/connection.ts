a37215e61832b41c68eae20fe85ca99d5075d6d2
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-09 19:40:29 +0100

    Refactor database connection code

--- server/src/connection.ts
+++ server/src/connection.ts
@@ -1,14 +1,10 @@
 import path from "path";
 import knex from "knex";
 
-const db_file_name: string = "dine_ready.db";
-const db_dir_name: string = "db";
-const db_path: string = path.resolve(__dirname, db_dir_name, db_file_name);
-
 export const db = knex({
     client: "sqlite3",
     connection: {
-        filename: db_path,
+        filename: path.resolve(__dirname, "db", "dine_ready.db"),
     },
     useNullAsDefault: true,
 });
