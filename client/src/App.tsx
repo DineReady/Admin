869e39568e6e3f400fa5d92cde9d1c0c193e9d58
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-09 19:49:06 +0100

    Add useEffect hook to fetch orders from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import { CreateOrder, Header, Main } from "./components";
 import { mock } from "./lib";
+import { IOrder } from "./types";
 
 export default function App(): JSX.Element {
+    const [orders, setOrders] = useState<IOrder[]>();
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setLoading(true);
+                const response = await fetch("http://localhost:8080/orders");
+                const data = await response.json();
+                setOrders(data);
+                console.log(data);
+                setLoading(false);
+            } catch (error: unknown) {
+                console.error((error as Error).message);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, []);
+
     return (
         <>
             <Header orders={mock.length} />
-            <Main orders={mock} />
+            <Main orders={orders || mock} />
             <CreateOrder />
         </>
     );
