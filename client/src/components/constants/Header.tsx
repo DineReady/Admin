52e4715a46b1770291fb936cb60f63f29fde1b91
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-04-02 18:05:19 +0200

    Add Kinde authentication and Express dependencies

--- client/src/components/constants/Header.tsx
+++ client/src/components/constants/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { IHeader } from "../../types";
 import { getCurrentTime } from "../../lib";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 
 const Header = ({ orders }: IHeader): JSX.Element => {
     const [currTime, setCurrTime] = useState<string>("");
@@ -12,11 +13,17 @@ const Header = ({ orders }: IHeader): JSX.Element => {
 
         return (): void => clearInterval(interval);
     }, []);
-
+    const { login, register } = useKindeAuth();
     return (
         <div className="h-20 px-10 py-5 flex justify-between items-center">
             <div className="text-2xl font-black">ORDERS: {orders}</div>
             <div className="text-2xl font-black">{currTime}</div>
+            <button onClick={() => register()} type="button">
+                Register
+            </button>
+            <button onClick={() => login()} type="button">
+                Log In
+            </button>
         </div>
     );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,16 +5,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 const root: Root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <ChakraProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-            </Routes>
-        </BrowserRouter>
-    </ChakraProvider>,
+    <KindeProvider
+        clientId="4f2cabe28e574ae2b291b275bcaa0867"
+        domain="https://dineready.kinde.com"
+        redirectUri="http://localhost:3000"
+        logoutUri="http://localhost:3000"
+    >
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    </KindeProvider>,
 );
 
 reportWebVitals();
