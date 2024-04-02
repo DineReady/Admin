510f485d44a343a73287539b260e01f2d3512c1c
Oskar Pietrzak <u114_oskpie_waw@technischools.com>
2024-04-02 12:41:03 +0200

    lr nwm czy dobrze

--- client/src/components/orders/Main.tsx
+++ client/src/components/orders/Main.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { IMain, IOrder, OrderStatus } from "../../types";
 import AllOrders from "./all_orders/AllOrders";
+import App from "../../App";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const Main = ({ orders }: IMain): JSX.Element => {
     const [ordersByStatus, setOrdersByStatus] = useState<{
@@ -8,26 +10,41 @@ const Main = ({ orders }: IMain): JSX.Element => {
     }>();
 
     useEffect(() => {
-        const groupedOrders = orders?.reduce(
-            (acc, order) => {
-                acc[order.status] = [...(acc[order.status] || []), order];
-                return acc;
-            },
-            {} as { [key in OrderStatus]: IOrder[] },
-        );
+        const fetchData = async () => {
+            const groupedOrders = orders?.reduce(
+                (acc, order) => {
+                    acc[order.status] = [...(acc[order.status] || []), order];
+                    return acc;
+                },
+                {} as { [key in OrderStatus]: IOrder[] },
+            );
 
-        setOrdersByStatus(groupedOrders);
+            setOrdersByStatus(groupedOrders);
+        };
+
+        fetchData();
     }, [orders]);
 
+    const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+    if (!PUBLISHABLE_KEY) {
+        throw new Error("Missing Publishable Key");
+    }
+
     return (
-        <div className="h-full w-screen grid grid-cols-2">
-            {ordersByStatus &&
-                Object.entries(ordersByStatus).map(
-                    ([status, orders]): JSX.Element => (
-                        <AllOrders key={status} orders={orders} status={status as OrderStatus} />
-                    ),
-                )}
-        </div>
+        <React.StrictMode>
+            <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                <App />
+            </ClerkProvider>
+            <div className="h-full w-screen grid grid-cols-2">
+                {ordersByStatus &&
+                    Object.entries(ordersByStatus).map(
+                        ([status, orders]): JSX.Element => (
+                            <AllOrders key={status} orders={orders} status={status as OrderStatus} />
+                        ),
+                    )}
+            </div>
+        </React.StrictMode>
     );
 };
 
