44debfe716b1fc207d949909e732f592b66d42ea
Oskar <129163056+dewjpl@users.noreply.github.com>
2024-04-15 22:14:12 +0200

    Delete Admin-10 directory

--- Admin-10/server/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import allOrders from "./all_orders";
-import createOrder from "./create_order";
-import validateOrderId from "./validate_order_id";
-import orderDetails from "./order_details";
-import updateState from "./update_state";
-import deleteOrder from "./delete_order";
-
-export { allOrders, createOrder, validateOrderId, orderDetails, updateState, deleteOrder};
