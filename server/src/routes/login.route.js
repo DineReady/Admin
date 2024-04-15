83fd2502dffe367c424b4a3415d550d53533b64c
Oskar <129163056+dewjpl@users.noreply.github.com>
2024-04-15 22:04:24 +0200

    Delete server directory

--- server/src/routes/login.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginService = require('./service');
-
-router.post('/api/admin/login', loginService.adminLogin);
-
-module.exports = router;
--- server/src/routes/users.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersService = require('./service');
-
-router.get('/api/users', usersService.getUsers);
-router.post('/api/users', usersService.addUser);
-
-module.exports = router;
